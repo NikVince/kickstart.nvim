d901528eb601e82316214b87fcf237a96f8dbdb8
NikVince <165183152+NikVince@users.noreply.github.com>
2025-05-04 11:47:59 +0200

    UPDATED init.lua to add CAPS LOCK to ESC keybinding.

--- init.lua
+++ init.lua
@@ -185,6 +185,9 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 -- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 -- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
+-- Add an easy-to-reach insert mode exit using 'jk'
+vim.keymap.set('i', 'jk', '<Esc>', { noremap = true, silent = true, desc = 'Exit insert mode with jk' })
+
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
